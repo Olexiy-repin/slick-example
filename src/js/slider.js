bc69fa1a1fc2afbc040028b92ae08cac589ebbd8
Olexiy-repin <repinlyoha@gmail.com>
2021-09-01 10:33:04 +0300

    сделан слайдер в секции reviews

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        dots: true,
+        dotsClass:'reviews__dots list',
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
